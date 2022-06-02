01490d1708ee3a075285ad5fe59cdbb4d5bd371e
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-02 16:55:08 +0300

    create basemodel class and init function

---
+++ Backend/model/basemodel.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Basemodel for rest of the models"""
+
+from datetime import datetime
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+Base = declarative_base()
+
+class BaseModel:
+    """Base model class """
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(String(60), default=datetime.utcnow, nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        """Initialize a new model"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        
+        if kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
