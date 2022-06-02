779a5ad274c64015e3a4b1105478d3ad36301717
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-02 18:25:27 +0300

    add db initializer

--- Backend/models/engine/db_handler.py
+++ Backend/models/engine/db_handler.py
@@ -6,3 +6,13 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 class DBHandler:
     """DB handler class"""
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initialize db engine"""
+        self.__engine = create_engine(
+                'mysql+mysqldb://{}:{}@{}:3306/{}'
+                .format('root', 'root', 'localhost', 'story'),
+                pool_pre_ping=True)
+
