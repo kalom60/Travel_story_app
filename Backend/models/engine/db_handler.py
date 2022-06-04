f348da8d657c3af753808e7df90d2fb3f2a3b69c
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-04 13:24:18 +0300

    initialize database and create function that return all objects from a class

--- Backend/models/engine/db_handler.py
+++ Backend/models/engine/db_handler.py
@@ -3,6 +3,9 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from models.basemodel import Base
+from models.user import User
+from models.story import Story
 
 class DBHandler:
     """DB handler class"""
@@ -13,6 +16,21 @@ class DBHandler:
         """Initialize db engine"""
         self.__engine = create_engine(
                 'mysql+mysqldb://{}:{}@{}:3306/{}'
-                .format('root', 'root', 'localhost', 'story'),
+                .format('root', 'root', 'localhost', 'story_db'),
                 pool_pre_ping=True)
+
+    def all(self, cls=None):
+        """Return all objects in a class"""
+        dict = {}
+        for obj in self.__session.query(cls).all():
+            key = type(obj).__name__ + '.' + obj.id
+            dict[key] = obj
+        return dict
+
+    def reload(self):
+        """launch the database"""
+        Base.metadata.create_all(self.__engine)
+        session_factor = sessionmaker(bind=self.__engine)
+        Session = scoped_session(session_factor)
+        self.__session = Session()
 
