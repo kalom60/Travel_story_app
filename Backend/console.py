4ecc01e19cd6fa3cc3f702b47af0ddf517c0ba34
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-04 15:11:08 +0300

    add exit function

--- Backend/console.py
+++ Backend/console.py
@@ -2,11 +2,22 @@
 """console program for managing backends using python"""
 
 import cmd
+from models.basemodel import BaseModel
+from models.user import User
+from models.story import Story
+from models import storage
 
 class Command(cmd.Cmd):
     """Console"""
     prompt = '(App) '
 
+    def do_quit(self, arg):
+        """Exit the console"""
+        return True
+
+    def emptyline(self):
+        """an empty line + Enter should execute nothing"""
+        pass
 
 if __name__ == '__main__':
     Command().cmdloop()
