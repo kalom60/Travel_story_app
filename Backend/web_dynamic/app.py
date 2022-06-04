d4aac3b18717521cba46cfab2d219571c3e61029
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-04 13:24:43 +0300

    flask app

---
+++ Backend/web_dynamic/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Flask app """
+
+from flask import Flask
+from models import storage
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """index page"""
+    return "Hello Flask"
+
+if __name__ == '__main__':
+    app.run(debug=True)
