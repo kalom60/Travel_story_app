c5d181fa8ef08658559e136489cbc19230deb09b
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-04 16:57:16 +0300

    add a function that prints all object from class

--- Backend/console.py
+++ Backend/console.py
@@ -1,30 +1,52 @@
-#!/usr/bin/python3
+#!/opt/homebrew/bin/python3
 """console program for managing backends using python"""
 
 import cmd
-from models.basemodel import BaseModel
 from models.user import User
 from models.story import Story
 from models import storage
 
+
 class Command(cmd.Cmd):
     """Console"""
     prompt = '(App) '
+    __classes = {
+        User.__name__: User,
+        Story.__name__: Story
+    }
 
     def do_quit(self, arg):
         """Exit the console"""
         return True
 
     def emptyline(self):
-        """an empty line + Enter should execute nothing"""
+        """
+        an empty line + Enter should execute nothing
+        """
         pass
 
     @staticmethod
     def parse(line):
-        """split all args and return list"""
+        """
+        split all args and return list
+        """
         if len(line) == 0:
             return []
         return line.split(' ')
+
+    def do_all(self, arg):
+        """
+        prints all object of a class
+        Ex: $ all User
+        """
+        args = Command.parse(arg)
+        if len(args) == 0:
+            print('** class name missing **')
+        else:
+            if args[0] in Command.__classes.keys():
+                print(storage.all(Command.__classes[args[0]]))
+            else:
+                print('** class doesn\'t exist **')
 
 
 
