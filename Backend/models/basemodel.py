b6f682766faeff980e3d2b75cc6c9d55b13b0d63
kalom60 <39136564+kalom60@users.noreply.github.com>
2022-06-04 17:00:38 +0300

    change datatype of created_at column

--- Backend/models/basemodel.py
+++ Backend/models/basemodel.py
@@ -2,26 +2,26 @@
 """Basemodel for rest of the models"""
 
 from datetime import datetime
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
 
 Base = declarative_base()
 
+
 class BaseModel:
     """Base model class """
     id = Column(String(60), nullable=False, primary_key=True)
-    created_at = Column(String(60), default=datetime.utcnow, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
 
     def __init__(self, *args, **kwargs):
         """Initialize a new model"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.utcnow()
-        
+
         if kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
-
 
     def to_dict(self):
         """return a dictionary all instances of the class"""
@@ -31,4 +31,3 @@ class BaseModel:
             del dict['_sa_instance_state']
         dict['created_at'] = self.created_at.isoformat()
         return dict
-        
